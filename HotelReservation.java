a09aaf94cab20f7b480a4bc70ae3c7ce2d42b038
diksha-kalra <kalradiksha11@gmail.com>
2020-10-12 23:30:40 +0530

    Added: displaying welcome to hotel reservation program

---
+++ HotelReservation.java
@@ -0,0 +1,8 @@
+package com.cg.hotelreservationprogram;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Hotel Reservation Program");
+	}
+}
